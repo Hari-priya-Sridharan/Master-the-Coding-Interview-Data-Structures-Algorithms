3d491f9bb6e0a868b5de378900fc5428cde16fc3
Hari-priya-Sridharan <65731962+Hari-priya-Sridharan@users.noreply.github.com>
2023-04-14 17:48:18 +0000

    29. What Is Good Code?_1
    Find Nemo

---
+++ findnemo.java
@@ -0,0 +1,14 @@
+public class findnemo {
+
+  public static void main(String[] args) {
+     String[] array=new String[]{"Nemo"};
+    findNemo(array);
+  }
+  public static void findNemo(String[] array){
+    for(int i=0;i<array.length;i++){
+      if(array[i].equals("Nemo")){
+        System.out.println("Found NEMO!");
+      }
+    }
+  }
+}
